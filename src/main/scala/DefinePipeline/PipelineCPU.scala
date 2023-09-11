100edee50b419e6ec935f3da80e09c8c18001eb6
xie-1399 <3161932956@qq.com>
2023-09-11 15:31:38 +0800

    learn some thing about the pipeline examples

---
+++ src/main/scala/DefinePipeline/PipelineCPU.scala
@@ -0,0 +1,131 @@
+package DefinePipeline
+
+  /*
+  using the spinal lib to create a simple 5 stages pipeline which can deal the Riscv-arith inst
+  fetch -> decode -> excute -> memory -> writeback
+  this is only show a way to use the pipeline
+  */
+import DefineSim.SpinalSim.{PrefixComponent, RtlConfig}
+import spinal.core._
+import spinal.lib._
+import spinal.lib.pipeline.Connection.M2S
+import spinal.lib.pipeline._
+
+import scala.collection.mutable.ArrayBuffer
+
+object Constant{
+  val instructionWidth = 32
+  val Xlen = 32
+}
+
+case class signal() extends Bundle {} //or can set pipeline regs signal here
+
+class PipelineCPU extends PrefixComponent{
+  import Constant._
+  val io = new Bundle{
+    val inst = slave Stream(Bits(instructionWidth bits))
+    val halt = in Bool()
+    val flush = in Bool()
+  }
+
+  val pipeline = new Pipeline {
+    val fetch = new Stage().setName("Fetch")
+    val decode = new Stage().setName("Decode")
+    val excute = new Stage().setName("Excute")
+    val memory = new Stage().setName("Memory")
+    val writeback = new Stage().setName("WriteBack")
+
+    val PC = Stageable(UInt(Xlen bits))
+    val Inst = Stageable(Bits(instructionWidth bits))
+    val Halt = Stageable(Bool())
+    val Flush = Stageable(Bool())
+    val rs1 = Stageable(UInt(5 bits))
+    val rs2 = Stageable(UInt(5 bits))
+    val rd = Stageable(UInt(5 bits))
+    val op1 = Stageable(Bits(Xlen bits))
+    val op2 = Stageable(Bits(Xlen bits))
+    val alu_res = Stageable(UInt(Xlen bits))
+    val imm = Stageable(UInt(12 bits)) //may be set signal
+    val useimm = Stageable(Bool())
+
+    import Connection._
+
+    connect(fetch, decode)(M2S())
+    connect(decode, excute)(M2S())
+    connect(excute, memory)(M2S())
+    connect(memory, writeback)(M2S())
+
+    // fetch stage just control the pc value
+    val onFetch = new Area {
+      import fetch._
+      Halt := io.halt
+      Flush := io.flush
+      Inst := io.inst.payload
+      val inc = RegInit(False)
+      val pc = Reg(UInt(Xlen bits)).init(0x80000000l)
+      val pcNext = Mux(inc, pc + 4 , pc)
+      PC := pcNext
+
+      when(!Halt && !Flush){
+        inc := True
+      }
+      when(Halt){
+        inc := False
+        fetch.haltIt()
+      }
+      when(Flush){
+        inc := False
+        fetch.flushIt()
+      }
+      io.inst.ready := fetch.isReady
+      valid := io.inst.valid
+    }
+
+    //fetch stage get the
+    val onDecode = new Area {
+      import decode._
+      //only for add and addi
+
+      rs1 := decode(Inst)(19 downto 15).asUInt
+      rs2 := decode(Inst)(24 downto 20).asUInt
+      rd := decode(Inst)(11 downto 7).asUInt
+      imm := decode(Inst)(31 downto 20).asUInt
+      val func3 = decode(Inst)(14 downto 12)
+      val func7 = decode(Inst)(31 downto 25)
+      val opcode = decode(Inst)(6 downto 0)
+      useimm := False
+      when(opcode === B"0010011"){
+        useimm := True
+      }
+
+      assert(func3.asUInt === 0)
+      assert(opcode === B"0110011" || opcode === B"0010011")
+
+      val array:ArrayBuffer[BigInt] = new ArrayBuffer[BigInt]()
+      for(idx <- 0 until 32) array += 0.toBigInt
+      val regfile  = Mem(Bits(32 bits),32).initBigInt(array)
+
+      op1 := regfile.readAsync(rs1,writeFirst)
+      op2 := regfile.readAsync(rs2,writeFirst)
+    }
+
+    val onExcute = new Area {
+      import excute._
+      when(excute(useimm)){
+        excute(alu_res) := (excute(imm) + excute(op1).asUInt).resized
+      }.otherwise{
+        excute(alu_res) := (excute(op1).asUInt + excute(op2).asUInt).resized
+      }
+    }
+
+    val onMemory = new Area {
+      //no memory operation
+    }
+
+    val onwriteBack = new Area {
+      import writeback._
+      onDecode.regfile.write(writeback(rd),writeback(alu_res).asBits,internals.input.valid)
+    }
+  }
+  pipeline.build()
+}
--- src/main/scala/DefinePipeline/PipelineExamples.scala
+++ src/main/scala/DefinePipeline/PipelineExamples.scala
@@ -4,43 +4,110 @@ import DefineSim.SpinalSim.PrefixComponent
 import spinal.core._
 import spinal.lib._
 import spinal.lib.pipeline._
-/*
- use the pipeline lib examples and think about it
- more function should look the source code of the pipeline lib
-*/
+import spinal.lib.pipeline.Connection._
+  /*
+   use the pipeline lib examples and think about it
+   more function should look the source code of the pipeline lib
+   here is the three simple examples about pipeline
+  */
 
-class PipelinewithPC extends PrefixComponent{
-  val io = new Bundle{
-    val halt = in Bool()
-    val flush = in Bool()
+  /*
+  insert regs in pipeline and use the stage reg
+  */
+class ExampleOne extends PrefixComponent{
+  val a, b = in UInt (8 bits)
+  val result = out UInt (8 bits)
+
+  implicit val pip = new Pipeline
+  val stageA = new Stage()(pip)  //implicit use the pipeline
+  val stageB = new Stage(connection = M2S())
+  val stageC = new Stage(connection = M2S())
+
+  val SUM = stageA.insert(a + b)
+  result := stageC(SUM)
+
+  pip.build()
+}
+
+  /*
+  in each stage can use stageable to check about it
+ */
+
+class ExampleTwo extends PrefixComponent{
+  val a, b = in UInt (8 bits)
+  val result = out Bool()
+
+  val pip = new Pipeline {
+    val SUM = Stageable(UInt(8 bits))
+    val CHECK = Stageable(Bool())
+
+    val stageA = new Stage {
+      SUM := a + b
+    }
+    val stageB = new Stage(connection = M2S()) {
+      CHECK := SUM < 5
+    }
+    val stageC = new Stage(connection = M2S()) {
+      result := CHECK
+    }
+    val check = stageC(CHECK)
+  }
+  pip.build()
+}
+
+class ExampleThree extends PrefixComponent{
+  val io = new Bundle {
+    val source = slave Stream (UInt(8 bits))
+    val sink = master Flow (UInt(8 bits))
+
+    val cond0 = in UInt (8 bits)
   }
 
-  val pipeline = new Pipeline{ //create the pipeline
-    val s0,s1,s2 = new Stage()
-    val PC = Stageable(UInt(32 bits))
-    val flush = Stageable(Bool())
-    val halt = Stageable(Bool())
+  val pipeline = new Pipeline {
+    val s0, s1, s2 = new Stage()
+    val A, B, C = Stageable(UInt(8 bits))
 
-    //connect the stage
     import Connection._
-    connect(s0,s1)(M2S())
-    connect(s1,s2)(M2S())
-    val resetValue = Reg(UInt(32 bits)) init 0x80000000l
 
-    val S0 = new Area {
-      import s0._  //should import with it
-      PC := resetValue
-      resetValue := resetValue + 4
-      s0.haltIt(io.halt)
-      s1.flushIt(io.flush)
+    connect(s0, s1)(M2S())
+    connect(s1, s2)(M2S())
+
+    val onS0 = new Area {
+
+      import s0._
+
+      valid := io.source.valid
+      io.source.ready := s0.isReady
+      A := io.source.payload
     }
 
-    val S1 = new Area {
+    val onS1 = new Area {
       import s1._
-      val valid = internals.output.valid
+      when(io.cond0 === 0) {
+        haltIt()
+      }
+      when(io.cond0 === 2) {
+        flushNext()
+      }
+
+      B := A + 1
+      s1(C, "miaou") := A + 2
+      (C, "wuff") := A + 3
+      (C, "wuff2") := A + 4
+
+      {
+        implicit val offset = new StageableOffset("yololo")
+        C := U(5)
+      }
+    }
+
+    val onS2 = new Area {
+      import s2._
+      io.sink.valid := internals.output.valid
+      io.sink.payload := B + s2(C, "miaou") + s2(C, "wuff") + (C, "wuff2") + (C, "yololo")
     }
 
   }
-
   pipeline.build()
+
 }
