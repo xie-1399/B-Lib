665311ebd85c12de980c6e3b2aaff9c20af26eed
xxl <3161932956@qq.com>
2023-10-20 10:43:54 +0800

    add counter init

--- src/main/scala/DefineUntils/Counter/CounterUntil.scala
+++ src/main/scala/DefineUntils/Counter/CounterUntil.scala
@@ -2,16 +2,22 @@ package DefineUntils.Counter
 
 import spinal.core._
 import spinal.lib._
-//all kinds of counter in the spinal lib
 
 object CounterUntil {
 
-  /*
-  two define types counter
-  */
+  /* two define types counter*/
   def cycleCounter(start:BigInt,end:BigInt) = DefineCounter(start,end,true)
   def keepCounter(start:BigInt,end:BigInt) = DefineCounter(start,end,false)
   def toFlow(defineCounter: DefineCounter): Flow[UInt] = defineCounter.toFlow()
   def init(defineCounter: DefineCounter,init:BigInt) = defineCounter.init(init)
+
+  /* for counters with init value */
+  def counterInit(Counters:Seq[Counter],init:BigInt) = {
+    for(counter <- Counters){
+      counter.init(init)
+    }
+
+  }
+
 
 }
